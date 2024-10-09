c122d4f3f6b6f9b996a4fb8016a31eb181f9cca8
Ujwal Srimanth <nusvarma@gmail.com>
2024-10-09 19:11:48 +0530

    inital_commit

---
+++ backend/controllers/auth.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import React from 'react'
+
+function auth() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default auth
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import HomePage from './components/homepage';
+import './App.css';
+function App() {
+  return (
+    <HomePage></HomePage>
+  );
+}
+
+export default App;
