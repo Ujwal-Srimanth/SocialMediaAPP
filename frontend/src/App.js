4119c911a51860f315b7c32cc70078e47516351a
Ujwal Srimanth <nusvarma@gmail.com>
2024-10-11 19:16:29 +0530

    ThirdCommit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
 import * as React from 'react';
-import './App.css';
+import {BrowserRouter,Navigate,Routes,Route} from 'react-router-dom';
+import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
+import { useMemo } from 'react';
+import {CssBaseline,ThemeProvider} from '@mui/material';
+import {createTheme} from '@mui/material/styles';
+import { themeSettings } from './theme';
+import { useSelector } from 'react-redux';
+
 function App() {
+  const mode = useSelector((state)=>state.mode)
+  const theme = useMemo(()=>createTheme(themeSettings(mode)),[mode])
   return (
-    <div>
-      
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+        <Routes>
+          <Route path="/" element={<LoginPage></LoginPage>}></Route>
+          <Route path="/home" element={<HomePage></HomePage>}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage></ProfilePage>}></Route>
+        </Routes>
+        </CssBaseline>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
